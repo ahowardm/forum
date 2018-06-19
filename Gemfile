ef018adad14ff1c422c96fba41027723bbcc3185
Andres Howard <ahowardm@gmail.com>
2018-06-19 15:29:21 -0400

    bootstrap, devise and oauth working

--- Gemfile
+++ Gemfile
@@ -36,6 +36,20 @@ gem 'jbuilder', '~> 2.5'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+# JQuery
+gem 'jquery-rails'
+
+# Bootstrap
+gem 'bootstrap-sass'
+
+# Devise
+gem 'devise'
+
+# OAuth
+gem 'therubyracer'
+gem 'omniauth'
+gem 'omniauth-facebook'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,28 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require jquery
+//= require bootstrap-sprockets
+
+
+function moveFooter() {
+
+    var docHeight = $(window).height();
+    var footerHeight = $('.footer').height();
+    var copyrightHeight = $('.copyright-row').height();
+    var footerTop = $('.footer').position().top + footerHeight + copyrightHeight;
+
+    if (footerTop < docHeight) {
+        $('.footer').css('margin-top', 10 + (docHeight - footerTop) + 'px');
+    }
+}
+
+$(document).on('turbolinks:load', moveFooter);
+
+let resizeListener;
+let pause = 100;
+
+$(window).resize(function() {
+    clearTimeout(resizeListener);
+    resizeListener = setTimeout(moveFooter, pause);
+});
